f817bca3fb5335aa893d0c0a08dc00d880d15e46
suraj basnet <surajbasnet449@gmail.com>
2022-03-18 11:28:23 -0400

    first commit

---
+++ h1.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo rand(0 , 9) . '<br>';
+echo rand(100 , 500) . '<br>';
+echo rand(200, 400) . '<br>';
+
+?>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+  
+<div>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://www.pexels.com/photo/yellow-animal-eyes-fur-41315/" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.pexels.com/photo/brown-lion-cub-under-blue-and-white-sky-624033/" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.pexels.com/photo/man-and-woman-holding-hands-walking-on-seashore-during-sunrise-1024960/" class="d-block w-100" alt="">
+    </div>
+  </div>
+</div>
+</div>
+<div>
+<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+  <?php  include'h1.php';?>
+  <div class="card-body">
+    <h5 class="card-title">Lucky Number</h5>
+  </div>
+</div>
+<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+  <?php  include'h1.php';?>
+  <div class="card-body">
+    <h5 class="card-title">Beautiful Number</h5>
+  </div>
+</div>
+<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+  <?php  include'h1.php';?>
+  <div class="card-body">
+    <h5 class="card-title">Worst Number</h5>
+  </div>
+</div>
+<div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+  <?php  include'h1.php';?>
+  <div class="card-body">
+    <h5 class="card-title">coolest  Number</h5>
+  </div>
+</div>
+
+</div>
+
+
+
+
+
+    <!-- js file -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
